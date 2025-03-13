ef3a206e588d6b79ae24c3d11ae76aa15a0a23ce
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-03-13 19:54:03 +0100

    003_numeric_data.py

---
+++ 003_numeric_data.py
@@ -0,0 +1,31 @@
+data_1 = 12
+data_2 = 5.323
+
+print(data_1 + data_2)
+print(data_1 - data_2)
+print(data_1 * data_2)
+print(data_1 / data_2)
+print(data_1 // data_2)
+print(data_1 ** data_2)
+print(data_1 % data_2)
+
+print((10 + 3) *(2 **2) / 4 - 1)
+
+print(abs(-15))
+
+print(round(data_2))
+print(round(data_1))
+
+print(data_1 == data_2)
+print(data_1 != data_2)
+print(data_1 >  data_2)
+print(data_1 <  data_2)
+print(data_1 >= data_2)
+print(data_1 <= data_2)
+
+print(int(data_2))
+print(float(data_1))
+
+
+print(int("20"))
+
