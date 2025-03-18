08e93ee3b93d320cb3c548b6e5a640b98ac7780f
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-03-18 10:41:21 +0100

    006_input_arithmetic.py

---
+++ 006_input_arithmetic.py
@@ -0,0 +1,14 @@
+j = input("Enter ist number")
+k = input("Enter 2nd number")
+s = j + k
+print(s)
+
+j = int(input("Enter ist number"))
+k = float(input("Enter 2nd numbe"))
+s = j + k
+print(s)
+
+
+result = eval(input('enter an expr'))
+print(result)
+
