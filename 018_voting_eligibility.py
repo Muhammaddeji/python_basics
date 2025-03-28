f4c0e2c68843e698f01cf8b7396a88169c315c27
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-03-28 17:29:33 +0100

    018_voting_eligibility.py

---
+++ 018_voting_eligibility.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your Age:"))
+nationality = input("Enter your nationality:")
+
+if age  >= 18 and nationality == "Nigeria":
+
+    print("You are eligible to vote in Nigeria")
+
+if age  <18  and nationality ==  "Nigeria":
+
+    print("you are too young to vote")
+
+if age is not "Nigeria":
+
+    print("you must be a Nierian citizen to vote here")
---
+++ 019_age_clarification.py
@@ -0,0 +1,15 @@
+age = int(input("Enter your Age:"))
+
+if age < 13:
+    print("You are a child")
+
+elif  13  <= age <= 19:
+    print("you are a teenager")
+
+elif  20  <= age <= 59:
+    print("you are an adult")
+
+else:
+    print("you are a senior citizen")
+
+
