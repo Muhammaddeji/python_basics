18e8f0b199d992a7ccbdeaf3ee20e53a7f23ac90
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-04-05 13:43:52 +0100

    026_function.py

---
+++ 026_function.py
@@ -0,0 +1,23 @@
+def greet_user():
+    print("Hello, welcome to python programming!")
+
+greet_user()
+
+
+def add_numbers(a,b):
+    
+    y = a + b
+    
+    print(y)
+
+add_numbers(5, 7)
+
+
+
+def multiply_numbers(a,b):
+    return a * b
+
+result = multiply_numbers(5,7)
+
+print("result:", result)
+
