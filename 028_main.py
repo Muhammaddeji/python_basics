d8430ea8d75b86c5c69a76a07ff5c28e71e571f5
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-04-05 17:39:26 +0100

    028_main.py

---
+++ 028_main.py
@@ -0,0 +1,17 @@
+from math_027_operations import add, sub, multiply,divide
+
+a = 30
+b = 15
+
+c = add(a,b)
+print(c)
+
+c = sub(a,b)
+print(c)
+
+c = multiply(a,b)
+print(c)
+
+c = divide(a,b)
+print(c)
+
